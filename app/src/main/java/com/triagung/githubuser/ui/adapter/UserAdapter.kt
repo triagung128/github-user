6b9a98a3d72cfbeae5ce1af1d1fd9123d9c1c7ac
triagung128 <triagung128@gmail.com>
2021-12-02 04:12:03 +0700

    create model and adapter

---
+++ app/src/main/java/com/triagung/githubuser/ui/adapter/UserAdapter.kt
@@ -0,0 +1,40 @@
+package com.triagung.githubuser.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.triagung.githubuser.databinding.ItemUserBinding
+import com.triagung.githubuser.model.User
+
+class UserAdapter(private val dataList: List<User>, private val listener: Listener) :
+    RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(dataList[position])
+    }
+
+    override fun getItemCount(): Int = dataList.size
+
+    inner class ViewHolder(private val binding: ItemUserBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(data: User) {
+            binding.apply {
+                tvName.text = data.name
+                tvUsername.text = data.username
+                ivUser.setImageResource(data.avatar)
+            }
+
+            itemView.setOnClickListener {
+                listener.onItemClickListener(data)
+            }
+        }
+    }
+
+    interface Listener {
+        fun onItemClickListener(data: User)
+    }
+}
