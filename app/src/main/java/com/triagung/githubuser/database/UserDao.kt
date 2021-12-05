0777ddcba36c869d17a5efb34e2cee9a79a04157
triagung128 <triagung128@gmail.com>
2021-12-05 07:26:20 +0700

    create interface dao

---
+++ app/src/main/java/com/triagung/githubuser/database/UserDao.kt
@@ -0,0 +1,17 @@
+package com.triagung.githubuser.database
+
+import androidx.room.*
+import com.triagung.githubuser.model.User
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(user: User)
+
+    @Delete
+    fun delete(user: User)
+
+    @Query("SELECT * FROM user WHERE login = :login LIMIT 1")
+    fun getUserByUsername(login: String): User
+}
