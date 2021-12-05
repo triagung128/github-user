23199ee86e12fe9aa3cc233b295ce99394814067
triagung128 <triagung128@gmail.com>
2021-12-05 12:28:39 +0700

    create favorite activity

---
+++ app/src/main/java/com/triagung/githubuser/ui/activity/FavoriteActivity.kt
@@ -0,0 +1,72 @@
+package com.triagung.githubuser.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.triagung.githubuser.R
+import com.triagung.githubuser.databinding.ActivityFavoriteBinding
+import com.triagung.githubuser.model.UserDetail
+import com.triagung.githubuser.ui.adapter.FavoriteAdapter
+import com.triagung.githubuser.viewmodel.FavoriteViewModel
+import com.triagung.githubuser.viewmodel.ViewModelFactory
+
+class FavoriteActivity : AppCompatActivity(), FavoriteAdapter.Listener {
+
+    private lateinit var binding: ActivityFavoriteBinding
+    private lateinit var favoriteViewModel: FavoriteViewModel
+    private lateinit var favoriteAdapter: FavoriteAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityFavoriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbar.setNavigationIcon(R.drawable.ic_round_arrow_back_24)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        favoriteViewModel = obtainViewModel(this)
+
+        favoriteAdapter = FavoriteAdapter(this)
+
+        setupRecyclerView()
+
+        favoriteViewModel.getAllUser().observe(this, { listUser ->
+            if (listUser.isNotEmpty()) {
+                favoriteAdapter.setData(listUser)
+                binding.tvNoFavorite.visibility = View.GONE
+                binding.rvFavorite.visibility = View.VISIBLE
+            } else {
+                binding.tvNoFavorite.visibility = View.VISIBLE
+                binding.rvFavorite.visibility = View.GONE
+            }
+        })
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): FavoriteViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(FavoriteViewModel::class.java)
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvFavorite.apply {
+            layoutManager = LinearLayoutManager(this@FavoriteActivity)
+            adapter = favoriteAdapter
+        }
+    }
+
+    override fun onItemClickListener(data: UserDetail) {
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra(DetailActivity.EXTRA_USERNAME, data.login)
+        startActivity(intent)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+}
