4837e3dfecef49d8162bba7debf6e2dc720e0990
triagung128 <triagung128@gmail.com>
2021-12-05 11:00:45 +0700

    fix entity

--- app/src/main/java/com/triagung/githubuser/model/UserDetail.kt
+++ app/src/main/java/com/triagung/githubuser/model/UserDetail.kt
@@ -8,40 +8,46 @@ import com.google.gson.annotations.SerializedName
 
 @Entity
 data class UserDetail(
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "id")
+    @Expose
+    @SerializedName("id")
+    var id: Int = 0,
+
     @ColumnInfo(name = "avatar_url")
     @Expose
     @SerializedName("avatar_url")
-    var avatarUrl: String = "",
+    var avatarUrl: String? = null,
     
     @ColumnInfo(name = "bio")
     @Expose
     @SerializedName("bio")
-    var bio: String = "",
+    var bio: String? = null,
     
     @ColumnInfo(name = "blog")
     @Expose
     @SerializedName("blog")
-    var blog: String = "",
+    var blog: String? = null,
     
     @ColumnInfo(name = "company")
     @Expose
     @SerializedName("company")
-    var company: String = "",
+    var company: String? = null,
     
     @ColumnInfo(name = "created_at")
     @Expose
     @SerializedName("created_at")
-    var createdAt: String = "",
+    var createdAt: String? = null,
     
     @ColumnInfo(name = "email")
     @Expose
     @SerializedName("email")
-    var email: String = "",
+    var email: String? = null,
     
     @ColumnInfo(name = "events_url")
     @Expose
     @SerializedName("events_url")
-    var eventsUrl: String = "",
+    var eventsUrl: String? = null,
     
     @ColumnInfo(name = "followers")
     @Expose
@@ -51,7 +57,7 @@ data class UserDetail(
     @ColumnInfo(name = "followers_url")
     @Expose
     @SerializedName("followers_url")
-    var followersUrl: String = "",
+    var followersUrl: String? = null,
     
     @ColumnInfo(name = "following")
     @Expose
@@ -61,58 +67,52 @@ data class UserDetail(
     @ColumnInfo(name = "following_url")
     @Expose
     @SerializedName("following_url")
-    var followingUrl: String = "",
+    var followingUrl: String? = null,
     
     @ColumnInfo(name = "gists_url")
     @Expose
     @SerializedName("gists_url")
-    var gistsUrl: String = "",
+    var gistsUrl: String? = null,
     
     @ColumnInfo(name = "gravatar_id")
     @Expose
     @SerializedName("gravatar_id")
-    var gravatarId: String = "",
+    var gravatarId: String? = null,
     
     @ColumnInfo(name = "hireable")
     @Expose
     @SerializedName("hireable")
-    var hireable: String = "",
+    var hireable: String? = null,
     
     @ColumnInfo(name = "html_url")
     @Expose
     @SerializedName("html_url")
-    var htmlUrl: String = "",
-    
-    @PrimaryKey
-    @ColumnInfo(name = "id")
-    @Expose
-    @SerializedName("id")
-    var id: Int = 0,
+    var htmlUrl: String? = null,
     
     @ColumnInfo(name = "location")
     @Expose
     @SerializedName("location")
-    var location: String = "",
+    var location: String? = null,
     
     @ColumnInfo(name = "login")
     @Expose
     @SerializedName("login")
-    var login: String = "",
+    var login: String? = null,
     
     @ColumnInfo(name = "name")
     @Expose
     @SerializedName("name")
-    var name: String = "",
+    var name: String? = null,
     
     @ColumnInfo(name = "node_id")
     @Expose
     @SerializedName("node_id")
-    var nodeId: String = "",
+    var nodeId: String? = null,
     
     @ColumnInfo(name = "organizations_url")
     @Expose
     @SerializedName("organizations_url")
-    var organizationsUrl: String = "",
+    var organizationsUrl: String? = null,
     
     @ColumnInfo(name = "public_gists")
     @Expose
@@ -127,12 +127,12 @@ data class UserDetail(
     @ColumnInfo(name = "received_events_url")
     @Expose
     @SerializedName("received_events_url")
-    var receivedEventsUrl: String = "",
+    var receivedEventsUrl: String? = null,
     
     @ColumnInfo(name = "repos_url")
     @Expose
     @SerializedName("repos_url")
-    var reposUrl: String = "",
+    var reposUrl: String? = null,
     
     @ColumnInfo(name = "site_admin")
     @Expose
@@ -142,30 +142,30 @@ data class UserDetail(
     @ColumnInfo(name = "starred_url")
     @Expose
     @SerializedName("starred_url")
-    var starredUrl: String = "",
+    var starredUrl: String? = null,
     
     @ColumnInfo(name = "subscriptions_url")
     @Expose
     @SerializedName("subscriptions_url")
-    var subscriptionsUrl: String = "",
+    var subscriptionsUrl: String? = null,
     
     @ColumnInfo(name = "twitter_username")
     @Expose
     @SerializedName("twitter_username")
-    var twitterUsername: String = "",
+    var twitterUsername: String? = null,
     
     @ColumnInfo(name = "type")
     @Expose
     @SerializedName("type")
-    var type: String = "",
+    var type: String? = null,
     
     @ColumnInfo(name = "updated_at")
     @Expose
     @SerializedName("updated_at")
-    var updatedAt: String = "",
+    var updatedAt: String? = null,
     
     @ColumnInfo(name = "url")
     @Expose
     @SerializedName("url")
-    var url: String = ""
+    var url: String? = null
 )
